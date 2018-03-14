692b0187c98affd6aec5d90743b7f1172a98e2d8
Harshit Shetty <swaharsh@gmail.com>
2018-03-14 19:40:07 +0530

    Add [Username Box]

--- src/App.js
+++ src/App.js
@@ -2,31 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const API = 'https://api.github.com/users';
+// const API = 'https://api.github.com/users';
 
   let url = `https://api.github.com/users/ShettyHarshit`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data)
+      // console.log(data)
     })
     .catch((error) => console.log('Oops! . There Is A Problem'))
 
     class SearchBar extends React.Component {
       render() {
         return <div className="search--box">
-            <form >
-              <label>
-                <input type="search" ref="username" placeholder="Type Username + Enter" />
-              </label>
+         <form onSubmit={this.handleForm.bind(this)}>              
+              {/* <label> */}
+              <input type="search" ref="username" placeholder="Type Username + Enter" />
+              {/* </label> */}
             </form>
           </div>;
+      }
+
+      handleForm(e) {
+        e.preventDefault();
+        let username = this.refs.username.value;
+        console.log(username);
+        this.refs.username.value = "";
+        // this.props.fetchProfile(username);
+        // this.refs.username.getDOMNode().value = "";
       }
     }
 
     class FuckYou extends React.Component {
       render () {
-        return <p className="App-intro">Btw, Fuck You</p>;
+        return <p className="App-intro">Btw, SIUUUUUUUUUUUUU</p>;
       }
     }
 
