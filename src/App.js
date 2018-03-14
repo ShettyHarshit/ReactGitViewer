adf6358e1e3ce901298f2e69f6c9e7518a2c1654
Harshit Shetty <swaharsh@gmail.com>
2018-03-14 18:50:11 +0530

    Add [Search Bar Compo]

--- src/App.js
+++ src/App.js
@@ -12,17 +12,35 @@ const API = 'https://api.github.com/users';
     })
     .catch((error) => console.log('Oops! . There Is A Problem'))
 
+    class SearchBar extends React.Component {
+      render() {
+        return <div className="search--box">
+            <form >
+              <label>
+                <input type="search" ref="username" placeholder="Type Username + Enter" />
+              </label>
+            </form>
+          </div>;
+      }
+    }
+
+    class FuckYou extends React.Component {
+      render () {
+        return <p className="App-intro">Btw, Fuck You</p>;
+      }
+    }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Viewahh</h1>
+          <img src={gitlogo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Git-React Viewahh</h1>
         </header>
-        <p className="App-intro">
-          Btw, Fuck You
-        </p>
+        <FuckYou/>
+        <SearchBar/>
       </div>
     );
   }
