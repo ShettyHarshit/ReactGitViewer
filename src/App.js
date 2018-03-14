88d2463c808019f597959bf47fa9727e26f90631
Harshit Shetty <swaharsh@gmail.com>
2018-03-14 18:59:51 +0530

    Change [Logo]

--- src/App.js
+++ src/App.js
@@ -30,15 +30,20 @@ const API = 'https://api.github.com/users';
       }
     }
 
+    class HeaderSpinner extends React.Component {
+      render() {
+        return <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to Git-React Viewahh</h1>
+          </header>;
+      }
+    }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={gitlogo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Git-React Viewahh</h1>
-        </header>
+        <HeaderSpinner/>
         <FuckYou/>
         <SearchBar/>
       </div>
