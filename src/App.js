2018f2b8a1d4b91684918b188400fe8ea3bd4a0b
Harshit Shetty <swaharsh@gmail.com>
2018-03-14 17:39:18 +0530

    Add [API fetch]

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API = 'https://api.github.com/users';
+
+  let url = `https://api.github.com/users/ShettyHarshit`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((error) => console.log('Oops! . There Is A Problem'))
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Viewahh</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Btw, Fuck You
         </p>
       </div>
     );
